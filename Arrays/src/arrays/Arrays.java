65e2997bf7b7ca9fe6a17d3dceefa91cd8299f47
tonuidavies <tonuidavies@gmail.com>
2016-04-24 21:56:03 +0300

    arrays learning

---
+++ Arrays/src/arrays/Arrays.java
@@ -0,0 +1,21 @@
+
+package arrays;
+
+
+public class Arrays {
+
+    
+    public static void main(String[] args) {
+        double [] myList = { 1.9, 2.9, 3.4, 3.5
+        
+    };
+        for(int i = 0; i<myList.length; i++){}
+        double total = 0;
+        for(int i = 0; i< myList.length; i++){
+            total+=myList[i];{
+        }
+            System.out.println("Total is " + total);
+        }
+    }
+    
+}
