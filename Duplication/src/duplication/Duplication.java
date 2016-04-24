0aa4bb1bac826438703bf15ce69de20aa0d8a6f9
tonuidavies <tonuidavies@gmail.com>
2016-04-24 21:57:14 +0300

    java variables

---
+++ Duplication/src/duplication/Duplication.java
@@ -0,0 +1,17 @@
+
+package duplication;
+
+/**
+ *
+ * @author dave
+ */
+public class Duplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Variables/src/variables/Variables.java
@@ -0,0 +1,15 @@
+
+package variables;
+
+public class Variables {
+
+   
+    public static void main(String[] args) {
+   double x = 3.2;
+   int y= 10;
+   System.out.println(x);
+   System.out.println(y);
+   
+    }
+    
+}
